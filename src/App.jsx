c56e86b88f7e7a57077234dd578d3e74dd71679e
1234rsp <riteshpatil1796@gmail.com>
2026-01-27 19:59:07 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  var title = "React JS";
+  var year = 2026;
+  var age = 77;
+  var person = {
+    name: "Ritesh",
+    age: 21,
+    city: "Surat"
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>HELLO</h1>
+      <h1>Welcome to {title}</h1>
+      <h1>Year: {year}</h1>
+      <h2>User Details ..... </h2>
+      <h3>Name: {person.name}</h3>
+      <h3>Age: {person.age}</h3>
+      <h3>City: {person.city}</h3>
+      
+
+    </div>
   )
 }
 
